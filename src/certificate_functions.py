24f1814c494a63b3676801cbbfa89e63c4fff283
Diana Anton <diana.anton00@e-uvt.ro>
2023-06-06 18:55:09 +0300

    generate, extend, read and revoke working
        pick 0141f04 fixed generating chain certs
        pick aa24991 generate, extend, read and revoke working

--- src/certificate_functions.py
+++ src/certificate_functions.py
@@ -66,7 +66,7 @@ def generate_certificate(
         )
         issuer_subject_key_identifier = x509.SubjectKeyIdentifier.from_public_key(issuer_public_key)
 
-        if serial_number != 0:   
+        if serial_number == 0:   
             # Self-sign the root certificate
             builder = builder.serial_number(x509.random_serial_number()).add_extension(
                 x509.BasicConstraints(ca=True, path_length=None), critical=True
@@ -90,7 +90,7 @@ def generate_certificate(
             backend=default_backend(),
         )
     else:
-        if serial_number != 0:   
+        if serial_number == 0:
             # Self-sign the root certificate
             builder = builder.serial_number(x509.random_serial_number()).add_extension(
                 x509.BasicConstraints(ca=True, path_length=None), critical=True
@@ -124,121 +124,6 @@ def generate_certificate(
 
 
 # ------------------------------------------------------------------------------#
-
-
-"""
-def generate_certificate(subject_name: str, issuer_name: str, issuer_private_key: str,
-                         private_key_path: str, certificate_path: str,
-                         issuer_certificate=None
-                         ):
-    # Generate a private key
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-
-    # Create a subject for the certificate
-    subject = x509.Name([
-        x509.NameAttribute(NameOID.COMMON_NAME, subject_name),
-    ])
-
-    # Create a certificate builder
-    builder = x509.CertificateBuilder().subject_name(subject)
-
-    if issuer_certificate:
-        builder = builder.issuer_name(issuer_certificate.subject)
-    else:
-        builder = builder.issuer_name(issuer_name)
-
-    # Set validity for the certificate
-    valid_from = datetime.datetime.utcnow()
-    valid_to = valid_from + datetime.timedelta(days=365)
-
-    builder = builder.not_valid_before(valid_from).not_valid_after(valid_to)
-
-    # Generate the public key from the private key
-    public_key = private_key.public_key()
-
-    # Add the public key to the certificate builder
-    builder = builder.public_key(public_key)
-
-    if issuer_certificate:
-        # Add the issuer's certificate as the signer
-        builder = builder.serial_number(x509.random_serial_number()).add_extension(
-            x509.AuthorityKeyIdentifier.from_issuer_subject_key_identifier(
-                issuer_certificate),
-            critical=False
-        )
-        certificate = builder.sign(private_key=issuer_private_key, algorithm=hashes.SHA256(),
-                                   backend=default_backend())
-    else:
-        # Self-sign the root certificate
-        builder = builder.serial_number(x509.random_serial_number()).add_extension(
-            x509.BasicConstraints(ca=True, path_length=None), critical=True
-        )
-        certificate = builder.sign(private_key=private_key, algorithm=hashes.SHA256(),
-                                   backend=default_backend())
-
-    with open(private_key_path, 'wb') as f:
-        f.write(private_key)
-
-    with open(certificate_path, 'wb') as f:
-        f.write(certificate)
-"""
-# ------------------------------------------------------------------------------#
-
-# # Usage
-# root_subject_name = 'Root Certificate'
-# intermediate_subject_names = ['Intermediate Certificate 1', 'Intermediate Certificate 2']
-
-# generate_certificate_chain(root_subject_name, intermediate_subject_names)
-"""
-{
-    "root": {
-        "common_name": "Root Certificate",
-        "organization_name": "uvt",
-        "country_name": "ro",
-        "private_key_path": "root_private_key.pem",
-        "certificate_path": "root_certificate.pem",
-        "validity_period": 365,
-    },
-    "1": {
-        "common_name": "Intermediate Certificate 1",
-        "organization_name": "uvt",
-        "country_name": "ro",
-        "private_key_path": "priv-1.pem",
-        "certificate_path": "cert-1.pem",
-        "validity_period": 365,
-        "issuer_name": "Root Certificate",
-        "issuer_private_key": "root_private_key.pem",
-        "issuer_certificate": "root_certificate.pem"
-    },
-    "2": {
-        "common_name": "Intermediate Certificate 2",
-        "organization_name": "uvt",
-        "country_name": "ro",
-        "private_key_path": "priv-2.pem",
-        "certificate_path": "cert-2.pem",
-        "validity_period": 365,
-        "issuer_name": "Intermediate Certificate 1",
-        "issuer_private_key": "priv-1.pem",
-        "issuer_certificate": "cert-1.pem"
-        },
-    # generate one more final intermediate certificate
-    "3": {
-        "common_name": "Final Intermediate Certificate",
-        "organization_name": "uvt",
-        "country_name": "ro",
-        "private_key_path": "priv-3.pem",
-        "certificate_path": "cert-3.pem",
-        "validity_period": 365,
-        "issuer_name": "Intermediate Certificate 2",
-        "issuer_private_key": "priv-2.pem",
-        "issuer_certificate": "cert-2.pem"
-    },
-}
-"""
 
 
 def generate_certificate_chain(
@@ -311,8 +196,30 @@ def generate_certificate_chain(
 # ------------------------------------------------------------------------------#
 
 
+def read_certificate_data(certificate_path: str):
+    # define a function that loads the certificate from the file and reads all the data from it
+    with open(certificate_path, "rb") as f:
+        certificate = x509.load_pem_x509_certificate(f.read(), default_backend())
+        # print all the data neatly 
+        print("------------------------------------------------------------")
+        print("Certificate data:")
+        print("Version:", certificate.version)
+        print("Serial number:", certificate.serial_number)
+        print("Subject:", certificate.subject)
+        print("Issuer:", certificate.issuer)
+        print("Not valid before:", certificate.not_valid_before)
+        print("Not valid after:", certificate.not_valid_after)
+        print("Public key:", certificate.public_key())
+        print("Signature algorithm:", certificate.signature_algorithm_oid._name)
+        print("Signature:", certificate.signature)
+        print("Extensions:", certificate.extensions)
+        print("------------------------------------------------------------")
+
+# ------------------------------------------------------------------------------#
+
+
 def extend_certificate_life(
-    certificate_path, private_key_path, extended_certificate_path, validity_days
+    certificate_path: str, private_key_path: str, extended_certificate_path: str, validity_days: int
 ):
     # Load the certificate and private key
     with open(certificate_path, "rb") as f:
@@ -330,6 +237,16 @@ def extend_certificate_life(
     extended_not_before = current_not_before
     extended_not_after = current_not_after + datetime.timedelta(days=validity_days)
 
+    # print the current validity period
+    print("Current validity period:")
+    print("Not before:", current_not_before)
+    print("Not after:", current_not_after)
+
+    # print the new validity period
+    print("New validity period:")
+    print("Not before:", extended_not_before)
+    print("Not after:", extended_not_after)
+
     # Create a certificate builder with the updated validity period
     builder = (
         x509.CertificateBuilder()
@@ -344,6 +261,12 @@ def extend_certificate_life(
     for extension in certificate.extensions:
         builder = builder.add_extension(extension.value, extension.critical)
 
+    # add the serial number to the certificate
+    builder = builder.serial_number(certificate.serial_number)
+
+    # Add the public key from the original certificate
+    builder = builder.public_key(certificate.public_key())
+
     # Sign the new certificate with the private key
     extended_certificate = builder.sign(private_key, hashes.SHA256(), default_backend())
 
@@ -357,12 +280,10 @@ def extend_certificate_life(
         f.write(extended_certificate_pem)
 
     print("Certificate extended successfully!")
-
-
 # ------------------------------------------------------------------------------#
 
 
-def revoke_certificate(certificate_path, private_key_path, crl_path, revoked_cert_path):
+def revoke_certificate(certificate_path: str, private_key_path: str, crl_path: str, revoked_cert_path: str, revocation_date: datetime.datetime):
     # Load the certificate and private key
     with open(certificate_path, "rb") as f:
         certificate = x509.load_pem_x509_certificate(f.read(), default_backend())
@@ -381,19 +302,16 @@ def revoke_certificate(certificate_path, private_key_path, crl_path, revoked_cer
             certificate.issuer
         )
 
-        crl = (
-            crl_builder.last_update(certificate.not_valid_before)
-            .next_update(certificate.not_valid_after)
-            .build(private_key)
+        crl_builder = crl_builder.last_update(certificate.not_valid_before)
+        crl_builder = crl_builder.next_update(certificate.not_valid_after)
+        crl_builder = crl_builder.add_revoked_certificate(
+            x509.RevokedCertificateBuilder()
+            .serial_number(certificate.serial_number)
+            .revocation_date(revocation_date)
+            .build(default_backend())
         )
 
-    # Add the revoked certificate to the CRL
-    revoked_cert = (
-        x509.RevokedCertificateBuilder()
-        .serial_number(certificate.serial_number)
-        .build(default_backend())
-    )
-    crl = crl.add_revoked_certificate(revoked_cert)
+        crl = crl_builder.sign(private_key, hashes.SHA256(), default_backend())
 
     # Serialize the CRL
     crl_pem = crl.public_bytes(serialization.Encoding.PEM)
@@ -407,8 +325,6 @@ def revoke_certificate(certificate_path, private_key_path, crl_path, revoked_cer
         f.write(certificate.public_bytes(serialization.Encoding.PEM))
 
     print("Certificate revoked successfully!")
-
-
 # ------------------------------------------------------------------------------#
 
 # # Usage
--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ from certificate_functions import (
     generate_certificate_chain,
     extend_certificate_life,
     revoke_certificate,
+    read_certificate_data,
 )
-import json
+import json, datetime
 
 # declare the main function
 
@@ -16,7 +17,8 @@ def main():
     print("2. Generate a certificate chain")
     print("3. Extend the life of a certificate")
     print("4. Revoke a certificate")
-    print("5. Exit")
+    print("5. Read certificate data")
+    print("6. Exit")
     # take the user input
     choice = int(input("Enter your choice: "))
     # if the user enters 1
@@ -37,8 +39,8 @@ def main():
         common_name = "root"
         organization_name = "uvt"
         country_name = "ro"
-        private_key_path = "root_private_key_1.pem"
-        certificate_path = "root_certificate_1.pem"
+        private_key_path = "./certs/root_private_key_1.pem"
+        certificate_path = "./certs/root_certificate_1.pem"
         validity_period = 365
 
         # print all the values
@@ -76,28 +78,78 @@ def main():
         generate_certificate_chain(certificates_data)
     # if the user enters 3
     elif choice == 3:
-        # extend the life of a certificate
-        extend_certificate_life()
+        # # take the user input for the certificate path
+        # certificate_path = input("Enter the certificate path: ")
+        # # take the user input for the private key path
+        # private_key_path = input("Enter the private key path: ")
+        # # take the user input for the extended certificate path
+        # extended_certificate_path = input(
+        #     "Enter the extended certificate path: ")
+        # # take the user input for the validity days
+        # validity_days = int(input("Enter the validity days: "))
+        # generate some test values to be hardcoded
+        certificate_path = './certs/root_certificate_1.pem'
+        private_key_path = './certs/root_private_key_1.pem'
+        extended_certificate_path = './certs/extended_root_certificate_1.pem'
+        validity_days = 665
+
+        # print all the values
+        print("Certificate path: " + certificate_path)
+        print("Private key path: " + private_key_path)
+        print("Extended certificate path: " + extended_certificate_path)
+        print("Validity days: " + str(validity_days))
+
+        # extend the life of the certificate
+        extend_certificate_life(certificate_path, private_key_path, extended_certificate_path, validity_days)
     # if the user enters 4
     elif choice == 4:
+        # # take the user input for the certificate path
+        # certificate_path = input("Enter the certificate path: ")
+        # # take the user input for the private key path
+        # private_key_path = input("Enter the private key path: ")
+        # # take the user input for the crl path
+        # crl_path = input("Enter the crl path: ")
+        # # take the user input for the revoked certificate path
+        # revoked_cert_path = input("Enter the revoked certificate path: ")
+        # take the user input for the revocation year
+        # revocation_year = int(input("Enter the revocation year: "))
+        # take the user input for the revocation month
+        # revocation_month = int(input("Enter the revocation month: "))
+        # take the user input for the revocation day
+        # revocation_day = int(input("Enter the revocation day: "))
+        # convert the revocation date to a datetime object
+        # revocation_date = datetime.datetime(
+        #     revocation_year, revocation_month, revocation_day
+        # )
+
+        certificate_path = './certs/extended_root_certificate_1.pem'
+        private_key_path = './certs/root_private_key_1.pem'
+        crl_path = './crl/crl.pem'
+        revoked_cert_path = './crl/revoked_cert.pem'
+        revocation_date = datetime.datetime(2023, 6, 6)
+
         # revoke a certificate
-        revoke_certificate()
-    # if the user enters 5
+        revoke_certificate(certificate_path, private_key_path, crl_path, revoked_cert_path, revocation_date)
     elif choice == 5:
+        # get user input for the certificate path
+        # certificate_path = input("Enter the certificate path: ")
+
+        certificate_path = './certs/extended_root_certificate_1.pem'
+        # certificate_path = './crl/revoked_cert.pem'
+
+        # print the certificate path
+        print("Certificate path: " + certificate_path)
+
+        # read certificate data
+        read_certificate_data(certificate_path)
+    # if the user enters 5
+    elif choice == 6:
         # exit the program
         exit()
     # if the user enters anything else
     else:
         # print an error message
         print("Invalid choice!")
-
-    # # generate the certificate chain
-    # generate_certificate_chain('root', ['intermediate1', 'intermediate2'])
-
-    # # extend the certificate life
-    # extend_certificate_life('intermediate1_certificate.pem',
-    #                         'intermediate1_private_key.pem', 'extended_intermediate1_certificate.pem', 365)
-
 
 # call the main function
 if __name__ == "__main__":
